dea5dcc4711e230d541cbaa8bf3dfbd356b08d23
Squirrelbear <petermitchell0001@gmail.com>
2022-02-08 23:51:43 +1030

    Network messages and UI additions.

--- StargateGuessWho/Assets/Scripts/Networking/NetworkManager.cs
+++ StargateGuessWho/Assets/Scripts/Networking/NetworkManager.cs
@@ -6,27 +6,85 @@ using SimpleJSON;
 
 public class NetworkManager : MonoBehaviour
 {
+    public string playerAuth;
+    public string sessionCode;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetWebData("http://localhost:7000", "?action=createPlayer&playerName=Peter"));
+        //StartCoroutine(GetWebData("http://localhost:7000", "?action=createPlayer&playerName=Peter"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            CreatePlayerOnServer("Peter");
+        }
     }
 
-    void ProcessServerResponse(string rawResponse)
+    void CreatePlayerOnServer(string playerName)
+    {
+        var message = new NetworkMessage.CreatePlayerMessage(playerName);
+        StartCoroutine(GetWebData("http://localhost:7000", message));
+    }
+
+    void CreateSessionOnServer()
+    {
+        var message = new NetworkMessage.CreateServerMessage(playerAuth);
+        StartCoroutine(GetWebData("http://localhost:7000", message));
+    }
+
+    void StartSessionOnServer()
+    {
+        var message = new NetworkMessage.StartRoundMessage(playerAuth, sessionCode);
+        StartCoroutine(GetWebData("http://localhost:7000", message));
+    }
+
+    void SendCharacterCommand(int characterID, string command)
+    {
+        var message = new NetworkMessage.UpdateSelectionMessage(playerAuth, sessionCode, characterID, command);
+        StartCoroutine(GetWebData("http://localhost:7000", message));
+    }
+
+    void GetSessionState()
+    {
+        var message = new NetworkMessage.GetStateMessage(playerAuth, sessionCode);
+        StartCoroutine(GetWebData("http://localhost:7000", message));
+    }
+
+    void ProcessServerResponse(string rawResponse, NetworkMessage.MessageTemplate requestedMessage)
     {
         JSONNode node = JSONNode.Parse(rawResponse);
-        Debug.Log("Name: " + node["playerName"] + "\n" + "Auth: " + node["playerAuth"]);
+        if(node.HasKey("error"))
+        {
+            Debug.Log("ERROR: " + node["error"]);
+            // TODO: Maybe resend the request?
+            return;
+        } 
+        else if(requestedMessage is NetworkMessage.CreatePlayerMessage)
+        {
+            playerAuth = node["playerAuth"];
+            Debug.Log("Created player: " + playerAuth);
+            // TODO trigger an event
+            // TODO Remove this command after testing
+            CreateSessionOnServer();
+        }
+        else if (requestedMessage is NetworkMessage.CreateServerMessage)
+        {
+            sessionCode = node["sessionCode"];
+            Debug.Log("Created session: " + sessionCode);
+            // TODO trigger an event
+        }
+
+        //Debug.Log("Name: " + node["playerName"] + "\n" + "Auth: " + node["playerAuth"]);
     }
 
-    IEnumerator GetWebData(string address, string query)
+    IEnumerator GetWebData(string address, NetworkMessage.MessageTemplate message)
     {
-        UnityWebRequest www = UnityWebRequest.Get(address + query);
+        UnityWebRequest www = UnityWebRequest.Get(address + "?" + message.GetMessage());
         yield return www.SendWebRequest();
 
         if(www.result != UnityWebRequest.Result.Success)
@@ -37,7 +95,7 @@ public class NetworkManager : MonoBehaviour
         {
             Debug.Log(www.downloadHandler.text);
 
-            ProcessServerResponse(www.downloadHandler.text);
+            ProcessServerResponse(www.downloadHandler.text, message);
         }
     }
 }
--- StargateGuessWho/Assets/Scripts/Networking/NetworkMessage.cs
+++ StargateGuessWho/Assets/Scripts/Networking/NetworkMessage.cs
@@ -4,28 +4,115 @@ using UnityEngine;
 
 public class NetworkMessage
 {
-    public class CreatePlayerMessage
+    public abstract class MessageTemplate
     {
-
+        public abstract string GetMessage();
     }
 
-    public class CreateServerMessage
+    public class CreatePlayerMessage : MessageTemplate
     {
+        public CreatePlayerMessage(string playerName)
+        {
+            PlayerName = playerName;
+        }
 
+        public string PlayerName {get; set;}
+
+        public override string GetMessage()
+        {
+            return "action=createPlayer&playerName=" + PlayerName;
+        }
     }
 
-    public class JoinServerMessage
+    public class CreateServerMessage : MessageTemplate
     {
+        public CreateServerMessage(string playerAuth)
+        {
+            PlayerAuth = playerAuth;
+        }
 
+        public string PlayerAuth { get; set; }
+
+        public override string GetMessage()
+        {
+            return "action=createServer&playerAuth=" + PlayerAuth;
+        }
     }
 
-    public class UpdateSelectionMessage
+    public class JoinServerMessage : MessageTemplate
     {
+        public JoinServerMessage(string playerAuth, string sessionCode)
+        {
+            PlayerAuth = playerAuth;
+            SessionCode = sessionCode;
+        }
 
+        public string PlayerAuth { get; set; }
+        public string SessionCode { get; set; }
+
+        public override string GetMessage()
+        {
+            return "action=joinServer&playerAuth=" + PlayerAuth
+                + "&sessionCode=" + SessionCode;
+        }
     }
 
-    public class StartRoundMessage
+    public class UpdateSelectionMessage : MessageTemplate
     {
+        public UpdateSelectionMessage(string playerAuth, string sessionCode, int characterID, string characterAction)
+        {
+            PlayerAuth = playerAuth;
+            SessionCode = sessionCode;
+            CharacterID = characterID;
+            CharacterAction = characterAction;
+        }
 
+        public string PlayerAuth { get; set; }
+        public string SessionCode { get; set; }
+        public int CharacterID { get; set; }
+        public string CharacterAction { get; set; }
+
+        public override string GetMessage()
+        {
+            return "action=characterCommand&playerAuth=" + PlayerAuth
+                + "&sessionCode=" + SessionCode + "&characterID=" + CharacterID
+                + "&characterAction=" + CharacterAction;
+        }
+    }
+
+    public class StartRoundMessage : MessageTemplate
+    {
+        public StartRoundMessage(string playerAuth, string sessionCode)
+        {
+            PlayerAuth = playerAuth;
+            SessionCode = sessionCode;
+        }
+
+        public string PlayerAuth { get; set; }
+        public string SessionCode { get; set; }
+
+        public override string GetMessage()
+        {
+            return "action=startGame&playerAuth=" + PlayerAuth
+                + "&sessionCode=" + SessionCode;
+        }
+    }
+
+    public class GetStateMessage : MessageTemplate
+    {
+        public GetStateMessage(string playerAuth, string sessionCode)
+        {
+            PlayerAuth = playerAuth;
+            SessionCode = sessionCode;
+        }
+
+        public string PlayerAuth { get; set; }
+        public string SessionCode { get; set; }
+
+        public override string GetMessage()
+        {
+            return "action=getState&playerAuth=" + PlayerAuth
+                + "&sessionCode=" + SessionCode;
+        }
     }
 }
